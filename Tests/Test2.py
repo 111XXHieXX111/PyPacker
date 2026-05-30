5a26f64336d90f2d13db0949e607f67a186be43e
XXHieXX <xxhiexx111@gmail.com>
2026-05-30 15:41:24 +0400

    Adding tests

---
+++ Tests/Test2.py
@@ -0,0 +1,8 @@
+value1 = int(input('Value 1:'))
+value2 = int(input('Value 2:'))
+
+print('Results')
+print(f'{value1}+{value2}={value1+value2}')
+print(f'{value1}-{value2}={value1-value2}')
+print(f'{value1}*{value2}={value1*value2}')
+print(f'{value1}/{value2}={value1/value2}')
